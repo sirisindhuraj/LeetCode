5f7a05f155eaecf3c0e7e9548b65b61f5cb78336
Siri Sindhu Raj <sirisindhuraj@gmail.com>
2026-02-25 20:20:30 +0530

    Binary_Search

---
+++ Binary_Search/Guess_number_higherOrLower.java
@@ -0,0 +1,42 @@
+/** 
+ * Forward declaration of guess API.
+ * @param  num   your guess
+ * @return 	     -1 if num is higher than the picked number
+ *			      1 if num is lower than the picked number
+ *               otherwise return 0
+ * int guess(int num);
+ */
+
+public class Solution extends GuessGame {
+    public int guessNumber(int n) {
+        int l=1;
+        int r=n;
+        while(l<=r){
+            int mid=l+(r-l)/2;
+            int res=guess(mid);
+            if(res==0){
+                return mid;
+            }else if(res==1){
+                l=mid+1;
+                
+            }
+            else{
+                r=mid-1;
+            }
+        }
+        return -1;
+    }
+}
+/**
+We are playing the Guess Game. The game is as follows:
+
+I pick a number from 1 to n. You have to guess which number I picked (the number I picked stays the same throughout the game).
+
+Every time you guess wrong, I will tell you whether the number I picked is higher or lower than your guess.
+
+You call a pre-defined API int guess(int num), which returns three possible results:
+
+-1: Your guess is higher than the number I picked (i.e. num > pick).
+1: Your guess is lower than the number I picked (i.e. num < pick).
+0: your guess is equal to the number I picked (i.e. num == pick).
+Return the number that I picked.*/
